79703462fdc60fea692b5d69461e65e678700034
cmcaquilala <cmcaquilala@up.edu.ph>
2023-11-22 14:53:26 +0800

    refactored code (from spaghetti OOP)

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,70 @@ import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import LetterButton from "./components/LetterButton";
 import {
-  initializeHangmanGame,
   answerLetter,
-  getWord,
+  getWordFromAPI,
   getGuess,
-  getPlayedLetters,
-  getNoOfMistakes,
+  checkWin,
+  checkLose,
 } from "./lib/hangman-game";
 
 function App() {
-  let letters: string[] = [];
-
-  for (let i = 65; i <= 90; i++) {
-    letters.push(String.fromCharCode(i));
-  }
-
-  const [wordToGuess, updateWordToGuess] = useState("-");
-  const [playedLetters, updatePlayedLetters] = useState<string[]>([]);
+  const [word, updateWord] = useState("");
+  const [guess, updateGuess] = useState("-");
+  const [playedLetters, updatePlayedLetters] = useState<Set<string>>(new Set());
   const [noOfMistakes, updateNoOfMistakes] = useState(0);
-  // const [isReady, setIsReady] = useState(false);
+  const [letters, updateLetters] = useState<string[]>([]);
+
+  useEffect(() => {
+    refreshGuess();
+  }, [word]);
+
+  const resetHangman = () => {
+    updateLetters([]);
+    for (let i = 65; i <= 90; i++) {
+      updateLetters((prevState) => [...prevState, String.fromCharCode(i)]);
+    }
+
+    getWordFromAPI().then((result) => {
+      updateWord(result);
+      refreshGuess();
+      updatePlayedLetters(new Set());
+      updateNoOfMistakes(0);
+    });
+  };
+
+  const refreshGuess = () => {
+    return updateGuess(getGuess(word, playedLetters));
+  };
 
   const pressLetter = (letter: string) => {
-    answerLetter(letter);
-    updatePlayedLetters(() => Array.from(getPlayedLetters()).sort());
-    updateNoOfMistakes(getNoOfMistakes());
-    updateWordToGuess(getGuess());
+    const [newPlayedLetters, newNoOfMistakes] = answerLetter(
+      letter,
+      word,
+      playedLetters,
+      noOfMistakes
+    );
+
+    updatePlayedLetters(newPlayedLetters);
+    updateNoOfMistakes(newNoOfMistakes);
+    refreshGuess();
+
+    if (checkWin(getGuess(word, newPlayedLetters))) {
+      alert("You win!");
+
+      resetHangman();
+    }
+
+    if (checkLose(newNoOfMistakes)) {
+      alert("You lost.");
+      alert("The word was " + word);
+
+      resetHangman();
+    }
   };
 
   useEffect(() => {
-    initializeHangmanGame().then(() => {
-      updateWordToGuess(getGuess());
-      // setIsReady(true);
-    });
+    resetHangman();
   }, []);
 
   return (
@@ -43,14 +75,16 @@ function App() {
       </div>
       <div className="row mx-0">
         <div className="col-5 bg-info">
-          hangman guy here
+          hangman guy here {word}
           <div>No of mistakes: {noOfMistakes}</div>
           <div>Played letters</div>
           <span>
-            {Array.from(playedLetters).map((letter) => " " + letter + " ")}
+            {Array.from(playedLetters)
+              .sort()
+              .map((letter) => " " + letter + " ")}
           </span>
         </div>
-        <div className="col-7 bg-warning">{wordToGuess}</div>
+        <div className="col-7 bg-warning">{guess}</div>
       </div>
       <div className="bg-success">
         {letters.map((letter) => (
--- src/lib/hangman-game.ts
+++ src/lib/hangman-game.ts
@@ -1,96 +1,42 @@
-let word = "nowordyet";
-let guess = "noguess";
-let noOfMistakes = 0;
-let playedLetters = new Set<string>();
 const NO_OF_ALLOWED_MISTAKES = 5; 
 
-export async function initializeHangmanGame() {
-  await getWordFromAPI().then((wordFromAPI) => {
-    setWord(wordFromAPI);
-    resetGuess(word);
-  });
-}
-
-async function getWordFromAPI() {
+export async function getWordFromAPI() {
   let response = await fetch("https://random-word-api.herokuapp.com/word");
-  let randomWord = (await response.json())[0];
+  let word = (await response.json())[0];
 
-  word = randomWord.toUpperCase();
-
-  return word;
+  return word.toUpperCase();
 }
 
-export function answerLetter(letter : string) {
-  if(getPlayedLetters().has(letter)) {
-    return;
-  }
-
-  let isInWord = getWord().includes(letter);
+export function answerLetter(letter : string, word : string, playedLetters : Set<string>, noOfMistakes : number) {
+  let isInWord = word.includes(letter);
 
   if(!isInWord) {
-    setNoOfMistakes(getNoOfMistakes() + 1);
+    noOfMistakes += 1;
   }
 
-  setPlayedLetters(getPlayedLetters().add(letter));
-  updateGuess();
+  playedLetters.add(letter);
 
-  if(checkLose()){
-    alert("You lost.");
+  return [
+    playedLetters,
+    noOfMistakes,
+  ] as const;
+}
+
+export function checkWin(guess : string) {
+  return !guess.includes("_");
+}
+
+export function checkLose(noOfMistakes : number) {
+  return noOfMistakes >= NO_OF_ALLOWED_MISTAKES;
+}
+
+
+export function getGuess(word : string, playedLetters : Set<string>) {
+  let guess = "";
+
+  for(let letter of word) {
+    guess += playedLetters.has(letter) ? ` ${letter}` : " _";
   }
 
-  if(checkWin()){
-    alert("You win!");
-  }
-}
-
-export function checkWin() {
-  return !getGuess().includes("_");
-}
-
-export function checkLose() {
-  return getNoOfMistakes() >= NO_OF_ALLOWED_MISTAKES
-}
-
-export function getWord() {
-  return word;
-}
-
-export function setWord(value : string) {
-  word = value;
-}
-
-export function getPlayedLetters() {
-  return playedLetters;
-}
-
-export function setPlayedLetters(value : Set<string>) {
-  playedLetters = value;
-}
-
-export function getGuess() {
   return guess;
-}
-
-export function updateGuess() {
-  guess = "";
-
-  for(let letter of getWord()) {
-    guess += getPlayedLetters().has(letter) ? ` ${letter}` : " _";
-  }
-}
-
-export function resetGuess(word : string) {
-  guess = "";
-
-  for(let i = 0; i < word.length; i++) {
-    guess += " _";
-  }
-}
-
-export function getNoOfMistakes() {
-  return noOfMistakes;
-}
-
-export function setNoOfMistakes(value : number) {
-  noOfMistakes = value;
 }
